4b009e5e72d1bab40268002987e7a74359ca0f8f
Zhimovskij <zhimovskij@yandex.ru>
2019-11-28 03:29:08 +0500

    make task 1

--- task1/app/Main.hs
+++ task1/app/Main.hs
@@ -1,29 +1,22 @@
 module Main where
 
 import Lib
-
-newtype Client = Client {
-    name :: String
-} deriving Show
-
-data Order = Order {
-    idOrder :: Int,
-    price :: Double,
-    nameClient :: String
-    }
-    
-parseChar :: String -> Int
-parseChar c = case c of
-                "0" -> 0
-                "1" -> 1
-                "2" -> 2
+import Train
+import Data.Time
 
 main :: IO ()
-main = do print "Enter number of order..."
-          input <- getLine
-          print $ nameClient $ orders !! parseChar input
-    where
-        clients  = [Client{name = "Andrey"}, Client{name = "Sergey"}, Client{name = "Artem"}]
-        orders = [Order{idOrder= 0, price = 45.0, nameClient = name (clients !! 1)},
-                 Order{idOrder = 1, price = 90.0, nameClient = name (head clients)},
-                 Order{idOrder = 2, price = 122.0, nameClient = name (clients !! 2)}]
+main = do number <- getLine
+          --print $ head [c | c <- trains, numberOfTrain c == (read number :: Int)]
+          print $ take 3 (sortTrainsOnDest trains)
+       where
+         trains = [
+             Train{destination = "Moscow", numberOfTrain = 3,
+                   departureTime = UTCTime (fromGregorian 2020 1 21) (timeOfDayToTime $ TimeOfDay 4 35 00)}
+             ,
+             Train{destination = "New Yourk", numberOfTrain = 4,
+                   departureTime = UTCTime (fromGregorian 2020 1 25) (timeOfDayToTime $ TimeOfDay 8 20 00)}
+             ,
+             Train{destination = "London", numberOfTrain = 1,
+                   departureTime = UTCTime (fromGregorian 2020 1 28) (timeOfDayToTime $ TimeOfDay 20 15 00)}
+                   ]
+         sortedTrains = sortTrains trains
---
+++ task1/app/Train.hs
@@ -0,0 +1,25 @@
+module Train where
+
+import Data.Time
+
+data Train = Train {
+    destination :: String,
+    numberOfTrain :: Int,
+    departureTime :: UTCTime
+} deriving Show
+
+-- |This function is to sort trains by their number.
+sortTrains :: [Train] -> [Train]
+sortTrains [] = []
+sortTrains (x:xs) = sortTrains [a | a <- xs, numberOfTrain a < numberOfTrain x]
+                                ++ [x] ++
+                    sortTrains [b | b <- xs, numberOfTrain b >= numberOfTrain x]
+
+-- |This function is to sort trains by destination place, if the destinations are equal, then by arrival time.
+sortTrainsOnDest :: [Train] -> [Train]
+sortTrainsOnDest [] = []
+sortTrainsOnDest (x:xs) = sortTrainsOnDest [a | a <- xs, (destination a < destination x) ??? (departureTime a < departureTime x)]
+                                            ++ [x] ++
+                          sortTrainsOnDest [b | b <- xs, (destination b >= destination x) ??? (departureTime b >= departureTime x)]
+                          where 
+                            (???) false _ = false
